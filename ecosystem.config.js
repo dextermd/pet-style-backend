7f2cd318653151cc5168ad0ab4f6c2a39980b884
dextermd <dextermdpay@gmail.com>
2025-01-06 15:44:58 +0200

    Small Fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'pet-style-backend',
       script: 'dist/src/main.js',
       instances: 1,
-      interpreter: 'node@20.18.1',
+      interpreter: '/root/.nvm/versions/node/v20.18.1/bin/node',
       autorestart: true,
       watch: true,
       max_memory_restart: '1G',
